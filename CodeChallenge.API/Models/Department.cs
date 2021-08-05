1a82ac7fec7b19851f1466a8969f9ee931a278ee
Erika Lobo <erikalobov@gmail.com>
2021-08-05 19:06:37 -0300

    Commit question 2

---
+++ CodeChallenge.API/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeChallenge.API.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public IList<Employee> Employees { get; set; }
+    }
+}

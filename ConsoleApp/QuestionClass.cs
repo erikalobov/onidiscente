30ca3a4f96ca20fe1bce1490c53af2b1ef7ca426
Erika Lobo <erikalobov@gmail.com>
2021-08-15 20:01:10 -0300

    Commit question 8

---
+++ ConsoleApp/QuestionClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public static class QuestionClass
+    {
+        static List<string> NamesList = new List<string>()
+        {
+            "Jimmy",
+            "Jeffrey",
+            "John",
+        };
+
+        public static void IterateNamesList()
+        {
+            int items = NamesList.Count;
+
+            while (items != 0)
+            {
+                Console.WriteLine(NamesList[items-1]);
+                items--;
+            }
+        }
+    }
+}
